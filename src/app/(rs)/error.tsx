4958d05801bb3e369a96f440a42798f1664278e0
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-12-07 00:09:22 +0200

    Log errors to Sentry for improved error tracking in Error component

--- src/app/(rs)/error.tsx
+++ src/app/(rs)/error.tsx
@@ -1,6 +1,7 @@
 'use client' // Error boundaries must be Client Components
  
 import { useEffect } from 'react'
+import * as Sentry from '@sentry/nextjs'
  
 export default function Error({
   error,
@@ -12,6 +13,7 @@ export default function Error({
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
+    Sentry.captureException(error)
   }, [error])
  
   return (
