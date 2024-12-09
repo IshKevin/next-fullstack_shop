ceacd6778ef9a76af5efd28ee97dba47ee2aa5b9
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-12-09 23:22:21 +0200

    Refactor login page layout and update login button implementation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,11 @@ import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
     return (
-        <main>
+        <main className="h-dvh flex flex-col items-center gap-6 text-4xl p-4">
         <h1>Repair shop</h1>
-        <Button onClick={() => LoginLink()}>Login</Button>
+        <Button asChild>
+            <LoginLink>Log in</LoginLink>
+        </Button>
         </main>
     );
 }
