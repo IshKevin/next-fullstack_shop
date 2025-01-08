38ff6d534236fb908382214068492d3bd847a69d
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2025-01-08 16:25:35 +0200

    feat: enhance CustomerForm with dynamic default values and improved state management

--- src/app/(rs)/customers/form/CustomerForm.tsx
+++ src/app/(rs)/customers/form/CustomerForm.tsx
@@ -4,39 +4,53 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Form } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
-import { InputWithLabel } from "@/components/inputs/InputWithLabel"
 
-import { insertCustomerSchema, type insertCustomerSchemaType, type selectCustomerSchemaType } from "@/zod-schemas/customer"
-import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel"
+import { InputWithLabel } from "@/components/inputs/InputWithLabel"
 import { SelectWithLabel } from "@/components/inputs/SelectWithLabel"
+import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel"
+import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel"
 
 import { StatesArray } from "@/constant/StatesArray"
 
-import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs"
-import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel"
-
+import { insertCustomerSchema, type insertCustomerSchemaType, type selectCustomerSchemaType } from "@/zod-schemas/customer"
 
 import { useAction } from 'next-safe-action/hooks'
 import { saveCustomerAction } from "@/app/actions/saveCustomerAction"
-import { useToast } from "@/hooks/use-toast"
-import { LoaderCircle } from "lucide-react"
+import { useToast } from '@/hooks/use-toast'
+import { LoaderCircle } from 'lucide-react'
 import { DisplayServerActionResponse } from "@/components/DisplayServerActionResponse"
 
-
-
-
+import { useSearchParams } from "next/navigation"
+import { useEffect } from "react"
 
 type Props = {
     customer?: selectCustomerSchemaType,
+    isManager?: boolean | undefined,
 }
 
-export default function CustomerForm({ customer }: Props) {
-    const {getPermission, isLoading} = useKindeBrowserClient()
-    const isManager = !isLoading && getPermission('manager')?.isGranted
+export default function CustomerForm({ customer, isManager = false }: Props) {
+
     const { toast } = useToast()
 
+    const searchParams = useSearchParams()
+    const hasCustomerId = searchParams.has("customerId")
 
-    const defaultValues: insertCustomerSchemaType = {
+    const emptyValues: insertCustomerSchemaType = {
+        id: 0,
+        firstName: '',
+        lastName: '',
+        address1: '',
+        address2: '',
+        city: '',
+        state: '',
+        zip: '',
+        phone: '',
+        email: '',
+        notes: '',
+        active: true,
+    }
+
+    const defaultValues: insertCustomerSchemaType = hasCustomerId ? {
         id: customer?.id ?? 0,
         firstName: customer?.firstName ?? '',
         lastName: customer?.lastName ?? '',
@@ -48,8 +62,8 @@ export default function CustomerForm({ customer }: Props) {
         phone: customer?.phone ?? '',
         email: customer?.email ?? '',
         notes: customer?.notes ?? '',
-        active:customer?.active ?? true,
-    }
+        active: customer?.active ?? true,
+    } : emptyValues
 
     const form = useForm<insertCustomerSchemaType>({
         mode: 'onBlur',
@@ -57,7 +71,11 @@ export default function CustomerForm({ customer }: Props) {
         defaultValues,
     })
 
-     const {
+    useEffect(() => {
+        form.reset(hasCustomerId ? defaultValues : emptyValues)
+    }, [searchParams.get("customerId")]) // eslint-disable-line react-hooks/exhaustive-deps
+
+    const {
         execute: executeSave,
         result: saveResult,
         isPending: isSaving,
@@ -82,13 +100,12 @@ export default function CustomerForm({ customer }: Props) {
     })
 
     async function submitForm(data: insertCustomerSchemaType) {
-        //console.log(data)
         executeSave(data)
     }
 
     return (
         <div className="flex flex-col gap-1 sm:px-8">
-            <DisplayServerActionResponse result={saveResult}/>
+            <DisplayServerActionResponse result={saveResult} />
             <div>
                 <h2 className="text-2xl font-bold">
                     {customer?.id ? "Edit" : "New"} Customer {customer?.id ? `#${customer.id}` : "Form"}
@@ -100,59 +117,71 @@ export default function CustomerForm({ customer }: Props) {
                     className="flex flex-col md:flex-row gap-4 md:gap-8"
                 >
                     <div className="flex flex-col gap-4 w-full max-w-xs">
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="First Name"
                             nameInSchema="firstName"
                         />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Last Name"
                             nameInSchema="lastName"
                         />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Address 1"
                             nameInSchema="address1"
                         />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Address 2"
                             nameInSchema="address2"
                         />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="City"
                             nameInSchema="city"
                         />
+
                         <SelectWithLabel<insertCustomerSchemaType>
                             fieldTitle="State"
                             nameInSchema="state"
                             data={StatesArray}
                         />
+
                     </div>
+
                     <div className="flex flex-col gap-4 w-full max-w-xs">
-                        
+
                         <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Zip"
+                            fieldTitle="Zip Code"
                             nameInSchema="zip"
                         />
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Phone"
-                            nameInSchema="phone"
-                        />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Email"
                             nameInSchema="email"
                         />
+
+                        <InputWithLabel<insertCustomerSchemaType>
+                            fieldTitle="Phone"
+                            nameInSchema="phone"
+                        />
+
                         <TextAreaWithLabel<insertCustomerSchemaType>
                             fieldTitle="Notes"
                             nameInSchema="notes"
                             className="h-40"
                         />
 
-                         {isLoading ? <p>Loading...</p> : isManager? (
+                        {isManager && customer?.id ? (
                             <CheckboxWithLabel<insertCustomerSchemaType>
                                 fieldTitle="Active"
                                 nameInSchema="active"
                                 message="Yes"
                             />) : null}
-                       <div className="flex gap-2">
+
+                        <div className="flex gap-2">
                             <Button
                                 type="submit"
                                 className="w-3/4"
@@ -160,18 +189,18 @@ export default function CustomerForm({ customer }: Props) {
                                 title="Save"
                                 disabled={isSaving}
                             >
-                                {isSaving?(
+                                {isSaving ? (
                                     <>
-                                    <LoaderCircle className="animate-spin"/> Saving
+                                        <LoaderCircle className="animate-spin" /> Saving
                                     </>
-                                ): "Save"}
+                                ) : "Save"}
                             </Button>
 
                             <Button
                                 type="button"
                                 variant="destructive"
                                 title="Reset"
-                                onClick={() =>{ 
+                                onClick={() => {
                                     form.reset(defaultValues)
                                     resetSaveAction()
                                 }}
@@ -179,8 +208,8 @@ export default function CustomerForm({ customer }: Props) {
                                 Reset
                             </Button>
                         </div>
+
                     </div>
-                    
 
                 </form>
             </Form>
