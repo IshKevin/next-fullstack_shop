8f635dc641dc556d6ddba805a73a394a11f54ee6
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2025-01-08 16:26:47 +0200

    feat: enhance error handling in safe-action client for unique entry validation

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,16 +1,26 @@
-import {  createSafeActionClient } from 'next-safe-action'
+import { createSafeActionClient } from 'next-safe-action'
 import { z } from 'zod'
 import * as Sentry from '@sentry/nextjs'
-import { metadata } from '@/app/layout'
+import type { NeonDbError } from '@neondatabase/serverless'
 
-export const actionClient = createSafeActionClient ({
-    defineMetadataSchema(){
+export const actionClient = createSafeActionClient({
+    defineMetadataSchema() {
         return z.object({
             actionName: z.string(),
         })
     },
     handleServerError(e, utils) {
         const { clientInput, metadata } = utils
+
+        if (e.constructor.name === 'NeonDbError') {
+            const { code, detail } = e as NeonDbError
+            if (code === "23505") {
+                // feedback displayed for user 
+                // not reported to Sentry
+                return `Unique entry required. ${detail}`
+            }
+        }
+
         Sentry.captureException(e, (scope) => {
             scope.clear()
             scope.setContext('serverError', { message: e.message })
@@ -18,9 +28,11 @@ export const actionClient = createSafeActionClient ({
             scope.setContext('clientInput', { clientInput })
             return scope
         })
+
         if (e.constructor.name === 'NeonDbError') {
             return "Database Error: Your data did not save. Support will be notified."
         }
+
         return e.message
     }
 })
